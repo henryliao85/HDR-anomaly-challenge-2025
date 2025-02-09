ca5c8ba0aaef935a657968d9e25181d8fb990828
Ken Lee <117441254+KenLee1130@users.noreply.github.com>
2025-02-09 17:43:22 +0800

    Update model.py

--- gravitational wave challenge/submission/model.py
+++ gravitational wave challenge/submission/model.py
@@ -1,25 +1,8 @@
+
+
 import torch
 from supervised_model_train import SimpleBinaryClassifier
-import numpy as np
 import os
-
-def minmax_scale(errors):
-    print(errors)
-    err_min = np.min(errors)
-    err_max = np.max(errors)
-    # 若 max==min，意義是所有誤差都一樣 => score都給0.5或0 
-    if err_max == err_min:
-        return np.zeros_like(errors)
-    return (errors - err_min) / (err_max - err_min)
-
-def normalize_data(X_test):
-    # 計算標準差
-    stds = np.std(X_test, axis=-1)[:, :, np.newaxis]
-    # 以標準差做除法 => 標準化
-    X_test = X_test / stds
-    # 轉軸
-    X_test = np.swapaxes(X_test, 1, 2)
-    return X_test
 
 class Model:
     def __init__(self):
@@ -48,24 +31,3 @@ class Model:
 
     def load(self):
         self.clf.load_state_dict(torch.load(os.path.join(os.path.dirname(__file__), 'supervied_model_fft_N_v3-4.pth'), map_location=self.device))
-
-if __name__ == "__main__":
-    data = np.load("test_data.npy")
-    root_dir = "/home/string-3/Documents/Hackathon/Datasets/"
-    dataset0 = normalize_data(np.load(root_dir+"background_test.npz")['data'])
-    dataset1 = normalize_data(np.load(root_dir+"bbh_for_challenge_test.npy"))#['data']
-    dataset2 = normalize_data(np.load(root_dir+"sglf_for_challenge_test.npy"))#['data']
-    
-    model = Model()
-    model.load()
-    #data = np.concatenate((dataset0[:100, :, :], dataset1[:100, :, :], dataset2[:100, :, :]))
-    #print(np.concatenate((data[0, :100, :, :], data[1, :100, :, :], data[2, :100, :, :])).shape)
-    #prediction = model.predict(data)
-    prediction = model.predict(dataset0[:100, :, :])
-    print(prediction)
-    print(len(prediction))
-    import matplotlib.pyplot as plt
-
-    plt.plot(prediction)
-    #plt.hist(prediction)
-    plt.show()
